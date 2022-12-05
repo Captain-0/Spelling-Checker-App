6af8e56cd34c3e3f71618990bbf1dfcda9be6b44
Captain-0 <sufiyan.in.tech+github@gmail.com>
2022-12-05 22:26:05 +0530

    defined root window

---
+++ main.py
@@ -0,0 +1,10 @@
+import tkinter
+from tkinter import *
+from textblob import TextBlob
+
+root = Tk()
+root.title("Spelling Checker")
+root.geometry("700x400")
+root.config(background="gray")
+
+root.mainloop()
