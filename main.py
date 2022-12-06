de38529549f068e329f1ab8fc0ee10bb0018bbca
Captain-0 <sufiyan.in.tech+github@gmail.com>
2022-12-06 22:52:22 +0530

    placed Heading

--- main.py
+++ main.py
@@ -7,4 +7,7 @@ root.title("Spelling Checker")
 root.geometry("700x400")
 root.config(background="gray")
 
+heading = Label(root, text="SPELLING CHECKER", font=("Trebuchet MS", 30, 'bold'), bg="gray", fg="#364971")
+heading.pack(pady=(50, 0))
+
 root.mainloop()
